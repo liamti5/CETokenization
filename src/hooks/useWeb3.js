a2ece79a3679d9cc40220258136e5b53abe97bae
Liam <liam.tessendorf@uzh.ch>
2023-07-07 11:07:31 +0200

    changed the sc to the one of the professor.

--- src/hooks/useWeb3.js
+++ src/hooks/useWeb3.js
@@ -7,7 +7,6 @@ const useWeb3 = () => {
   const [web3, setWeb3] = useState(null);
   const [account, setAccount] = useState(null);
   const [contract, setContract] = useState(null);
-  const [owners, setOwners] = useState(null);
   const { setEthereumAddress } = useEthereumAddress();
 
   useEffect(() => {
@@ -24,16 +23,9 @@ const useWeb3 = () => {
         setEthereumAddress(accounts[0]);
 
         // Load the smart contract
-        const contractAddress = '0xF70a640CDeDd456CdDaa51205A6Bc1bB9108EC3B';
+        const contractAddress = '0x9a039C5a20D89DAB69C8B12C53E55DE2464E5Cf3';
         const contract = new web3.eth.Contract(abi, contractAddress);
         setContract(contract);
-
-        // Load the owners
-        const issuers = await contract.methods._issuer().call();
-        const deputies = await contract.methods._deputy().call();
-        const remove = ["0x5a88f1E531916b681b399C33F519b7E2E54b5213"]
-        const owners = [...issuers, ...deputies, ...remove];
-        setOwners(owners);
       };
       loadWeb3AndBlockchainData();
     } else {
@@ -41,7 +33,7 @@ const useWeb3 = () => {
     }
   }, []);
 
-  return { web3, account, contract, owners };
+  return { web3, account, contract };
 };
 
 export default useWeb3;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,8 @@ import React from 'react';
 import Company from '../components/Company';
 import AppBar from '../components/AppBar';
 import StatusBar from '../components/StatusBar';
-import useWeb3 from '../hooks/useWeb3';
-import Loading from '../components/Loading';
 
 export default function Home() {
-  const { web3, account, contract, owners } = useWeb3();
-
-  const isLoading = !owners; // Check if owners data is not yet available
-
-  if (isLoading) {
-    return <Loading />; // Render a loading page while owners data is loading
-  }
-
   return (
     <>
       <AppBar />
